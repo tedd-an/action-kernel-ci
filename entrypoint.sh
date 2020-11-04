40ae36222de2795ef9ca46da9d4d2363037d707c
Tedd Ho-Jeong An <tedd.an@intel.com>
2020-11-04 15:18:27 -0800

    Fix bash syntax for test
    Using the correct syntax for testing.

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,7 @@ echo "HEAD-REF:   $GITHUB_HEAD_REF"
 echo "BASE-REF:   $GITHUB_BASE_REF"
 echo "PWD:        $(pwd)"
 
-if [[ -z $GITHUB_TOKEN ]]
-then
+if [ -z "$GITHUB_TOKEN" ]; then
 	echo "Set GITHUB_TOKEN environment variable"
 	exit 1
 fi
