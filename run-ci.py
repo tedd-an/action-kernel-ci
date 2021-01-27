6f6303b63c7c069a92d2eb37059a4c6544b87142
Tedd Ho-Jeong An <tedd.an@intel.com>
2021-01-26 23:09:03 -0800

    Fetch the number of commits to the source tree
    Currently, it only clones the tree with depth=1, and it causes an issue
    to run the gitlint or checkpatch due to missing commits.
    
    This patch fetches the number of commits in the  Pull request to the
    tree so those commits are accessable.

--- run-ci.py
+++ run-ci.py
@@ -947,6 +947,12 @@ def main():
         Path(output_dir).mkdir(parents=True, exist_ok=True)
         logger.debug("Created outputdirectory: %s" % output_dir)
 
+    logger.debug("Fetch commits in the tree: %d commits" % github_pr.commits)
+    pr_commits = github_pr.commits
+    logger.debug("Expand the commits in PR to the src: %d" % pr_commits)
+    proc = subprocess.Popen(('git', 'fetch', '--depth=%d' % pr_commits),
+                                 stdout=subprocess.PIPE, cwd=src_dir)
+
     # Run CI tests
     try:
         num_fails = run_ci(args)
